b4488012647c6dfe4824e79481f4472caca1445b
jonnybowen <60743016+jonnybowen@users.noreply.github.com>
2020-03-19 23:49:46 +0000

    New Screens
    Create study group
    study request
    leave feedback

---
+++ app/src/main/java/com/example/bestonealpha/createStudyOpp.java
@@ -0,0 +1,18 @@
+package com.example.bestonealpha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.os.Bundle;
+
+public class createStudyOpp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_study_opp);
+    }
+}
---
+++ app/src/main/java/com/example/bestonealpha/createStudyRequest.java
@@ -0,0 +1,14 @@
+package com.example.bestonealpha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createStudyRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_study_request);
+    }
+}
