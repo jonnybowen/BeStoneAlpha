98d0dceafc831c31292952d1521b7f2b681b6670
dmcvey <60473133+dmcvey@users.noreply.github.com>
2020-03-29 13:28:04 +0100

    adding create study opp button
    currently in profile page but can transfer this over to the search page

--- app/src/main/java/com/example/bestonealpha/searchActivity.java
+++ app/src/main/java/com/example/bestonealpha/searchActivity.java
@@ -1,62 +1,14 @@
 package com.example.bestonealpha;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Movie;
 import android.os.Bundle;
-import android.view.TextureView;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 public class searchActivity extends AppCompatActivity {
 
-    // zero arg constructor
-    public searchActivity() {
-
-    }
-
-
-    //Declare views
-    TextView userName;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        // Formatting the page
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_search);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         super.onCreate(savedInstanceState);
-        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
-            setTheme(R.style.AppDarkTheme);
-        } else {
-            setTheme(R.style.AppLightTheme);
-        }
-
-
-
-        //Retrieve user data from login screen
-        User loginUser = (User) getIntent().getSerializableExtra("user");
-        User currentUser = new User(loginUser.getEmail(), loginUser.getPassword());
-
-
-
-        userName = findViewById(R.id.userNamesrch);
-        userName.setText(currentUser.getDisplayName());
-
-        ImageButton profileImageButton = findViewById(R.id.searchProfilePicture);
-        profileImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent searchProfilePicture = new Intent(getApplicationContext(), profileActivity.class);
-                startActivity(searchProfilePicture);
-
-            }
-        });
+        setContentView(R.layout.activity_search);
     }
 }
